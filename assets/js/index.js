a664b8863448af653c4ec039ee2de9122a269f4c
Alexsandre <104799456+Lexkogake@users.noreply.github.com>
2022-06-13 15:33:54 -0300

    modificação na mascara de valores e no href

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,82 +2,11 @@ let limpaDados = document.querySelector('.limpa_dados');
 let formMercadoria = document.querySelector('#formMercadoria');
 let menuMobile = document.querySelector('.menu-mobile');
 let btnMenu = document.querySelector('.img-menu');
+let valor = document.querySelector('#valor');
 let items = JSON.parse(localStorage.getItem('items')) || [];
 
-function criaLista() {
-  let lista = document.querySelector('.listaLinha');
-  let tfoot = document.querySelector('.footer');
-  lista.innerHTML = '';
-  tfoot.innerHTML = '';
-  let total = 0;
-  let soma = 0;
-  for (const item of items) {
-    let tr = lista.insertRow();
-    let td_col1 = tr.insertCell();
-    let td_col2 = tr.insertCell();
-    let td_col3 = tr.insertCell();
-    td_col2.classList.add('positive');
-    td_col3.innerHTML = item.valor;
-
-    soma = item.valor.replace(/[^0-9,]*/g, '').replace(',', '.');
-    if (item.tipoTransacao == 'compra') {
-      total -= parseFloat(soma);
-      td_col2.innerHTML = ` -<span>${item.nomeMercadoria}</span>`;
-    } else {
-      total += parseFloat(soma);
-      td_col2.innerHTML = ` +<span>${item.nomeMercadoria}</span>`;
-    }
-  }
-
-  if (items.length == 0) {
-    let semConteudoTable = document.querySelector('.listaLinha');
-    semConteudoTable.innerHTML = `
-        <tr>
-          <td></td>
-          <td colspan='3'>
-            <p class='noConteudo'>
-              <span>Nenhuma transação cadastrada</span>
-            </p>
-          </td>
-        </tr>
-    `;
-  } else {
-    let ftoo = tfoot.insertRow();
-    let td_f1 = ftoo.insertCell();
-    let td_f2 = ftoo.insertCell();
-    let td_f3 = ftoo.insertCell();
-    ftoo.classList.add('tr-total');
-    td_f2.classList.add('space');
-    td_f3.classList.add('profit');
-
-    let totalMask =
-      'R$ ' +
-      parseFloat(total)
-        .toFixed(2)
-        .replace('.', ',')
-        .replace(/([0-9]*)([0-9]{3},*)/, '$1.$2');
-
-    td_f2.innerHTML = 'Total';
-
-    if (total > 0) {
-      td_f3.innerHTML = totalMask + '<span>[Lucro]</span>';
-    } else {
-      td_f3.innerHTML = totalMask + '<span>[Prejuizo]</span>';
-    }
-  }
-}
-
-const mask = {
-  money(value) {
-    const cleanValue = +value.replace(/\D+/g, '');
-    const options = { style: 'currency', currency: 'BRL' };
-    return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100);
-  }
-};
-
-let valor = document.querySelector('#valor');
 valor.addEventListener('input', element => {
-  element.target.value = mask.money(element.target.value);
+  element.target.value = valorMascara(element.target.value);
 });
 
 formMercadoria.addEventListener('submit', event => {
@@ -101,6 +30,7 @@ formMercadoria.addEventListener('submit', event => {
 
 limpaDados.addEventListener('click', () => {
   let text = 'Deseja limpar os dados ?';
+
   if (items.length > 0 && confirm(text) == true) {
     localStorage.removeItem('items');
     items = [];
@@ -117,5 +47,82 @@ btnMenu.addEventListener('click', () => {
 menuMobile.addEventListener('click', () => {
   menuMobile.classList.remove('ativo');
 });
+
+function criaLista() {
+  let tBodylista = document.querySelector('.listaLinha');
+  let tFoot = document.querySelector('.footer');
+  tBodylista.innerHTML = '';
+  tFoot.innerHTML = '';
+  let total = 0;
+  let soma = 0;
+  for (const item of items) {
+    let tr = tBodylista.insertRow();
+    let tdColuna1 = tr.insertCell();
+    let tdColuna2 = tr.insertCell();
+    let tdColuna3 = tr.insertCell();
+    tdColuna2.classList.add('positive');
+    tdColuna3.innerHTML = item.valor;
+
+    soma = item.valor.replace(/[^0-9,]*/g, '').replace(',', '.');
+    if (item.tipoTransacao == 'compra') {
+      total -= parseFloat(soma);
+      tdColuna2.innerHTML = ` -<span>${item.nomeMercadoria}</span>`;
+    } else {
+      total += parseFloat(soma);
+      tdColuna2.innerHTML = ` +<span>${item.nomeMercadoria}</span>`;
+    }
+  }
+
+  if (items.length == 0) {
+    let semConteudoTable = document.querySelector('.listaLinha');
+    semConteudoTable.innerHTML = `
+        <tr>
+          <td></td>
+          <td colspan='3'>
+            <p class='noConteudo'>
+              <span>Nenhuma transação cadastrada</span>
+            </p>
+          </td>
+        </tr>
+    `;
+  } else {
+    let footer = tFoot.insertRow();
+    let tdFooterColuna1 = footer.insertCell();
+    let tdFooterColuna2 = footer.insertCell();
+    let tdFooterColuna3 = footer.insertCell();
+    footer.classList.add('tr-total');
+    tdFooterColuna2.classList.add('space');
+    tdFooterColuna3.classList.add('profit');
+
+    let totalMask =
+      'R$ ' +
+      parseFloat(total)
+        .toFixed(2)
+        .replace('.', ',')
+        .replace(/([0-9]*)([0-9]{3},*)/, '$1.$2');
+
+    tdFooterColuna2.innerHTML = 'Total';
+
+    if (total == 0) {
+      console.log(total);
+      tdFooterColuna3.innerHTML = valorMascara(totalMask) + '<span></span>';
+    } else if (total > 0) {
+      console.log(total);
+      tdFooterColuna3.innerHTML =
+        valorMascara(totalMask) + '<span>[Lucro]</span>';
+    } else {
+      console.log(total);
+      tdFooterColuna3.innerHTML =
+        valorMascara(totalMask) + '<span>[Prejuizo]</span>';
+    }
+  }
+}
+
+function valorMascara(value) {
+  const valorString = value.toString();
+  const cleanValue = +valorString.replace(/\D+/g, '');
+  const options = { style: 'currency', currency: 'BRL' };
+  return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100);
+}
 
 criaLista();
